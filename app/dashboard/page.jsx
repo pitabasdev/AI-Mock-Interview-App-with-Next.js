d506ab074091ed50dd5c371897b1bb8ad2bcc9a4
Pitabas Pradhan <pitabas.pradhan834@gmail.com>
2024-11-15 00:37:50 +0530

    add the ADD new interview page inside dashboard

--- app/dashboard/page.jsx
+++ app/dashboard/page.jsx
@@ -1,11 +1,16 @@
 import { UserButton } from "@clerk/nextjs";
 import React from "react";
 import Header from "./_components/Header";
+import AddNewInterview from "./_components/AddNewInterview";
 
 function Dashboard() {
   return (
-    <div>
-      <Header />
+    <div className="p-10">
+     <h2 className="fonr-bold text-2xl">Dashboard</h2>
+     <h2 className="text-gray-500">Create and Start your Ai Mockup Interview</h2>
+     <div className="grid grid-cols-1 md:grid-cols-3 my-5">
+      <AddNewInterview/>
+     </div>
     </div>
   );
 }
