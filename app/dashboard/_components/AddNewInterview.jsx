95203601ff0492a1a148314478d1b9163d1d12c8
Pitabas Pradhan <pitabas.pradhan834@gmail.com>
2024-11-15 00:52:50 +0530

    add the dialog box

--- app/dashboard/_components/AddNewInterview.jsx
+++ app/dashboard/_components/AddNewInterview.jsx
@@ -1,11 +1,40 @@
-import React from 'react'
+"use client"
+import React, { useState } from "react";
+import {
+  Dialog,
+  DialogContent,
+  DialogDescription,
+  DialogHeader,
+  DialogTitle,
+
+} from "@/components/ui/dialog";
+import { Button } from "@/components/ui/button";
 
 function AddNewInterview() {
+    const[openDailog,setOpenDialog]=useState(false)
   return (
     <div>
-      
+      <div className="p-10 border rounded-lg bg-secondary hover:scale-105 hover:shadow-md cursor-pointer transition-all" onClick={()=>setOpenDialog(true)}>
+        <h2 className="text-lg text-center">+ Add New</h2>
+      </div>
+      <Dialog open={openDailog}>
+       
+        <DialogContent>
+          <DialogHeader>
+            <DialogTitle>Are you absolutely sure?</DialogTitle>
+            <DialogDescription>
+              This action cannot be undone. This will permanently delete your
+              account and remove your data from our servers.
+              <div className="flex gap-5 justify-end">
+                <Button variant="ghost" onClick={()=>setOpenDialog(false)}>Cancel</Button>
+                <Button>Start Interview</Button>
+              </div>
+            </DialogDescription>
+          </DialogHeader>
+        </DialogContent>
+      </Dialog>
     </div>
-  )
+  );
 }
 
-export default AddNewInterview
+export default AddNewInterview;
