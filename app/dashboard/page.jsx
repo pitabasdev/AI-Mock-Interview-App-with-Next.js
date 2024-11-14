e10c174089a5322cb1e78143b7e47f2fef7c364e
Pitabas Pradhan <pitabas.pradhan834@gmail.com>
2024-11-14 20:54:44 +0530

    add signin page

--- app/dashboard/page.jsx
+++ app/dashboard/page.jsx
@@ -1,9 +1,12 @@
+import { UserButton } from '@clerk/nextjs'
 import React from 'react'
+import Header from './_components/Header'
 
 function Dashboard() {
   return (
     <div>
-      
+        <Header/>
+      <UserButton/>
     </div>
   )
 }
